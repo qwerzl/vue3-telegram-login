fdcb3e43034ffbd5ee57756bfd18f3a3068e186c
qwerzl <46770502+qwerzl@users.noreply.github.com>
2022-12-13 22:59:54 +0800

    Update vue-telegram-login.vue

--- src/vue-telegram-login.vue
+++ src/vue-telegram-login.vue
@@ -1,68 +1,75 @@
 <template>
   <div ref="telegram"></div>
 </template>
-
-<script>
-export default {
-  name: 'vue-telegram-login',
-  props: {
+<script lang="ts" setup>
+  import { ref, onMounted } from 'vue';
+  const props = defineProps({
     mode: {
       type: String,
       required: true,
-      validator (value) { return ['callback', 'redirect'].includes(value) }
+      validator(value: string) {
+        return ['callback', 'redirect'].includes(value);
+      },
     },
     telegramLogin: {
       type: String,
       required: true,
-      validator (value) { return value.endsWith('bot') || value.endsWith('Bot') }
+      validator(value: string) {
+        return value.endsWith('bot') || value.endsWith('Bot');
+      },
     },
     redirectUrl: {
       type: String,
-      default: ''
+      default: '',
     },
     requestAccess: {
       type: String,
       default: 'read',
-      validator (value) { return ['read', 'write'].includes(value) }
+      validator(value: string) {
+        return ['read', 'write'].includes(value);
+      },
     },
     size: {
       type: String,
       default: 'large',
-      validator (value) { return ['small', 'medium', 'large'].includes(value) }
+      validator(value: string) {
+        return ['small', 'medium', 'large'].includes(value);
+      },
     },
     userpic: {
-      type: Boolean,
-      default: true
+      type: String,
+      default: 'true',
     },
     radius: {
-      type: String
-    }
-  },
-  methods: {
-    onTelegramAuth (user) {
-      this.$emit('callback', user)
-    }
-  },
-  mounted () {
+      type: String,
+    },
+  });
+  const emit = defineEmits(['callback']);
+  const onTelegramAuth = (user) => {
+    console.log('user-----', user);
+    emit('callback', user);
+  };
+  const telegram = ref();
+  onMounted(() => {
     // create script with given params
-    const script = document.createElement('script')
-    script.async = true
-    script.src = 'https://telegram.org/js/telegram-widget.js?3'
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://telegram.org/js/telegram-widget.js?3';
 
-    script.setAttribute('data-size', this.size)
-    script.setAttribute('data-userpic', this.userpic)
-    script.setAttribute('data-telegram-login', this.telegramLogin)
-    script.setAttribute('data-request-access', this.requestAccess)
-    if (this.radius) { script.setAttribute('data-radius', this.radius) }
-
-    if (this.mode === 'callback') {
-      window.onTelegramAuth = this.onTelegramAuth
-      script.setAttribute('data-onauth', 'window.onTelegramAuth(user)')
-    } else {
-      script.setAttribute('data-auth-url', this.redirectUrl)
+    script.setAttribute('data-size', props.size);
+    script.setAttribute('data-userpic', props.userpic);
+    script.setAttribute('data-telegram-login', props.telegramLogin);
+    script.setAttribute('data-request-access', props.requestAccess);
+    if (props.radius) {
+      script.setAttribute('data-radius', props.radius);
     }
 
-    this.$refs.telegram.appendChild(script)
-  }
-}
+    if (props.mode === 'callback') {
+      window.onTelegramAuth = onTelegramAuth;
+      script.setAttribute('data-onauth', 'window.onTelegramAuth(user)');
+    } else {
+      script.setAttribute('data-auth-url', props.redirectUrl);
+    }
+    telegram.value.appendChild(script);
+  });
 </script>
